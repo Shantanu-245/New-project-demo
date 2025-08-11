79685de7633080acf8175c7bfdedfb02da781815
Ankit Saraswat <ankitsaraswatyt@gmail.com>
2025-08-11 09:23:05 +0000

    edit

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+# Temporary storage (use DB later)
+users = {}
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if username in users and users[username] == password:
+            return f"Welcome, {username}!"
+        else:
+            return "Invalid username or password."
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if username in users:
+            return "Username already exists."
+        users[username] = password
+        return redirect(url_for('login'))
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
